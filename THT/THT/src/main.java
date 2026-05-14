518b739e937fd612884cd0709e2cb17f74612d3d
sultanzulvi <akundump27@gmail.com>
2026-05-14 14:04:28 +0700

    THT

--- THT/THT/src/main.java
+++ THT/THT/src/main.java
@@ -2,21 +2,21 @@ import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
-       
+        // Upcasting : Membuat daftar layanan ekspedisi dengan tipe data LayananEkspedisi untuk menyimpan berbagai jenis layanan
         ArrayList<LayananEkspedisi> daftarLayanan = new ArrayList<>();
-
+        
         daftarLayanan.add(new LayananReguler("REG-11", 2, 50, 50, 50)); 
         daftarLayanan.add(new LayananExpress("EXP-22", 5, 10, 10, 10)); 
         daftarLayanan.add(new LayananInternasional("INT-33", 3, 20, 20, 20, "Korea", 100));
 
-        double totalPendapatanPerusahaan = 0.0;
+        double totalPendapatanPerusahaan = 0.0; 
 
         for (LayananEkspedisi layanan : daftarLayanan) {
             layanan.cetakResi();
             
             double ongkirDasar = layanan.hitungOngkir();
             totalPendapatanPerusahaan += ongkirDasar;
-
+            // Downcasting : Memeriksa jenis layanan dan melakukan downcasting untuk mengakses metode khusus dari masing-masing jenis layanan
             if (layanan instanceof LayananReguler) {
                 LayananReguler reg = (LayananReguler) layanan;
                 double hargaPromo = reg.hitungOngkir(true, 25);
@@ -31,7 +31,7 @@ public class main {
                 inter.cetakManifest();
             }
         }
-
+        // Menampilkan total pendapatan perusahaan dari ongkir dasar yang dihitung untuk semua layanan
         System.out.println("\nTOTAL PENDAPATAN PERUSAHAAN (ONGKIR DASAR): Rp" + totalPendapatanPerusahaan);
     }
 }
